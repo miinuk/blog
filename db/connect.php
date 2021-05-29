73eb97585125f4c93367d35f6c6c90207f42f55b
willian <willianpereira39@hotmail.com>
2021-05-29 19:11:06 -0300

    começo site

---
+++ db/connect.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname   = "bd_escola";
+
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Conectado com Sucesso.";
+} catch(PDOException $e) {
+  echo "Conecção Falhou. " . $e->getMessage();
+}
+?>
